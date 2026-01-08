c8a6c43e51b2199473f7473c265b990bc5882c74
Andy <su27t34@gmail.com>
2026-01-08 22:57:34 +0800

    add toDoList node.js backend

---
+++ backend/src/errorHandle.js
@@ -0,0 +1,15 @@
+const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+}
+
+export default function errorHandle(res){
+    res.writeHead(400, headers);
+    res.write(JSON.stringify({
+        "status": "false",
+        "message": "欄位未填寫正確，或無值"
+    }))
+    res.end();
+}
